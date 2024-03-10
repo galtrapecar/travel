0bb6c8174d2098cbe469423d8e7130f2802a0cfb
gal trapecar <trapecar.gal@gmail.com>
2024-03-11 00:34:16 +0100

    sets up routing, helmets and login container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Navigate, Route, Routes as Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login/Login";
+import { Routes } from "./config";
 
 function App() {
-  return <div className="App"></div>;
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route path={Routes.Login} element={<Login />} />
+        <Route path="*" element={<Navigate to={Routes.Login} replace />} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+const Login = () => {
+  return (
+    <>
+      <Helmet>
+        <title>{"Login"}</title>
+      </Helmet>
+      <div className="Login"></div>
+    </>
+  );
+};
+
+export default Login;
