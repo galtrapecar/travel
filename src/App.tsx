7177fe0bbeab9b324683f929c8de39e4c66d9b77
Gal Trapečar <trapecar.gal@gmail.com>
2024-04-04 00:05:34 +0200

    Map setup (#1)
    * sets up routing, helmets and login container
    
    * adds maptiler maps
    
    * adds basic modal implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Navigate, Route, Routes as Switch } from "react-router-dom";
+import { Routes } from "./config";
+import Explore from "./pages/Explore/Explore";
 
 function App() {
-  return <div className="App"></div>;
+
+  return (
+    <div className="App">
+      <Switch>
+        {/* <Route path={Routes.Login} element={<Login />} /> */}
+        <Route path={Routes.Explore} element={<Explore />} />
+        <Route path="*" element={<Navigate to={Routes.Explore} replace />} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./root.scss";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Router>
+        <App />
+      </Router>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
